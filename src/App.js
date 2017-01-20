09d5b1df73100d0f466479c876c4d3230909d3b8
Jonathan Soape <jonathansoape@gmail.com>
2017-01-20 13:50:48 -0600

    Lesson 2 Start of searchable contact list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import ContactList from './ContactList.js';
+import SearchBar from './SearchBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Contact List!</h1>
+        <SearchBar />
+        <ContactList />
       </div>
     );
   }
