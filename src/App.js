50d3e9713481f1a53090628871bcae54c62ef794
Ivo Meißner <info@overtronic.com>
2017-01-21 15:04:31 -0600

    Added react / jsx eslint rules

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Contact List!</h1>
+        <h1>
+          Contact List!
+        </h1>
       </div>
     );
   }
