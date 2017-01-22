3148ff03e5d72e7b968ac2a2663df1ea6bf3f641
chrisperk <christopherperkins.512@gmail.com>
2017-01-21 20:08:24 -0600

    Contact List and Search Bar created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import ContactList from './ContactList.js';
+import SearchBar from './SearchBar.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>
-          Contact List!
-        </h1>
+        <SearchBar />
+        <ContactList />
       </div>
     );
   }
 }
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
 
 export default App;
