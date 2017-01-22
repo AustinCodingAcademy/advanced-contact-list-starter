eb884a3078700fc8730113381fda6a997bb4f4ed
dsolis421 <snapshot8d@gmail.com>
2017-01-21 20:24:17 -0600

    contact list initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import ContactList from './ContactList';
+import SearchBar from './SearchBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>
-          Contact List!
-        </h1>
+        <SearchBar />
+        <ContactList />
       </div>
     );
   }
