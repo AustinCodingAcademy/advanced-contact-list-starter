e547d4f8069641c8ec67059311441ce8e1c936d8
chrisperk <christopherperkins.512@gmail.com>
2017-01-22 19:07:33 -0600

    Contact List Search Bar functionality implemented

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,30 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class SearchBar extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      value: ''
-    };
-  }
-
-  handleChange(event) {
-    this.setState({
-      value: event.target.value
-    });
-  }
-
-  render() {
-    return (
-      <input
-        className="search-bar"
-        type="text"
-        value={this.state.value}
-        onChange={ (event) => this.handleChange(event) }
-      />
-    )
-  }
+const SearchBar = (props) => {
+  return (
+    <input
+      className="search-bar"
+      type="text"
+      value={props.value}
+      onChange={ (event) => props.onChange(event) }
+    />
+  );
 }
 
 export default SearchBar;
