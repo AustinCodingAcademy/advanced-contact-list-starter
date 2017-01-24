95e61876c1fede868464ebf8e5f7ba9b608adcf2
Brian Michael <brianmichael@Brians-MacBook-Pro.local>
2017-01-23 21:01:05 -0600

    working first version

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import ContactList from './ContactList';
+import SearchBar from './SearchBar';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>
-          Contact List!
-        </h1>
-      </div>
-    );
-  }
+
+    render() {
+
+        return (
+            <div className="App">
+                <SearchBar />
+                <ContactList />
+            </div>
+        );
+    }
+
 }
 
 export default App;
