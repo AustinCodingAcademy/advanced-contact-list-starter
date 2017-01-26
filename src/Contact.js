151f6ed266c8d30c543699cb4864a47a47454d3b
Jordan Mitchell <mordanjitchell@gmail.com>
2017-01-25 18:20:25 -0600

    React-ContactList

---
+++ src/Contact.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <li className="contact">
+        <div className="image-cropper">
+          <img src={this.props.avatar} alt="avatar" />
+        </div>
+        <div className="contact-info">
+        <h2>{this.props.name}</h2>
+      {this.props.occupation}
+      </div>
+      </li>
+    );
+  }
+}
+
+export default Contact;
---
+++ src/SearchBar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value: ''
+    };
+  }
+  handleChange(event) {
+    this.setState({
+      value: event.target.value
+    });
+  }
+  render() {
+    return (
+      <input
+        className="search-bar"
+        type="text"
+        value={this.state.value}
+        onChange={(event) => this.handleChange(event)}
+          />
+    );
+  }
+}
+
+export default SearchBar;
