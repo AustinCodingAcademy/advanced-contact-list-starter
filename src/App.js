026d32f3881b358bfb196222ef57091ff5994fc9
Madeline Olsen <madeline@Madelines-MacBook-Pro.local>
2017-02-02 14:56:07 -0600

    made searchbar for contact list functional

--- src/App.js
+++ src/App.js
@@ -1,47 +1,73 @@
-import React, { Component } from 'react';
-import ContactList from './ContactList';
-import SearchBar from './SearchBar';
-
+import React, { Component } from "react";
+import ContactList from "./ContactList";
+import SearchBar from "./SearchBar";
 
 class App extends Component {
-  render() {
-    const contacts = [
-      {
-        _id: 1,
-        name: 'Dale Cooper',
-        occupation: 'FBI Agent',
-        avatar: 'https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg'
-      },
-      {
-        _id: 2,
-        name: 'Spike Spiegel',
-        occupation: 'Bounty Hunter',
-        avatar: 'http://vignette4.wikia.nocookie.net/deadliestfiction/images/d/de/Spike_Spiegel_by_aleztron.jpg/revision/latest?cb=20130920231337'
-      },
-      {
-        _id: 3,
-        name: 'Wirt',
-        occupation: 'adventurer',
-        avatar: 'http://66.media.tumblr.com/5ea59634756e3d7c162da2ef80655a39/tumblr_nvasf1WvQ61ufbniio1_400.jpg'
-      },
-      {
-        _id: 4,
-        name: 'Michael Myers',
-        occupation: 'Loving little brother',
-        avatar: 'http://vignette2.wikia.nocookie.net/villains/images/e/e3/MMH.jpg/revision/latest?cb=20150810215746'
-      },
-      {
-        _id: 5,
-        name: 'Dana Scully',
-        occupation: 'FBI Agent',
-        avatar: 'https://pbs.twimg.com/profile_images/718881904834056192/WnMTb__R.jpg'
-      }
-    ];
+  constructor() {
+    super();
 
-    return (
+    this.state = {
+      searchText: "",
+      contacts: [
+        {
+          _id: 1,
+          name: "Dale Cooper",
+          occupation: "FBI Agent",
+          avatar: "https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg"
+        },
+        {
+          _id: 2,
+          name: "Spike Spiegel",
+          occupation: "Bounty Hunter",
+          avatar: "http://vignette4.wikia.nocookie.net/deadliestfiction/images/d/de/Spike_Spiegel_by_aleztron.jpg/revision/latest?cb=20130920231337"
+        },
+        {
+          _id: 3,
+          name: "Wirt",
+          occupation: "adventurer",
+          avatar: "http://66.media.tumblr.com/5ea59634756e3d7c162da2ef80655a39/tumblr_nvasf1WvQ61ufbniio1_400.jpg"
+        },
+        {
+          _id: 4,
+          name: "Michael Myers",
+          occupation: "Loving little brother",
+          avatar: "http://vignette2.wikia.nocookie.net/villains/images/e/e3/MMH.jpg/revision/latest?cb=20150810215746"
+        },
+        {
+          _id: 5,
+          name: "Dana Scully",
+          occupation: "FBI Agent",
+          avatar: "https://pbs.twimg.com/profile_images/718881904834056192/WnMTb__R.jpg"
+        }
+      ]
+    };
+  }
+
+  handleSearchBarChange(event) {
+    this.setState({
+      searchText: event.target.value
+    });
+  }
+
+  getFilteredContacts() {
+      // Remove any white space, and convert the searchText to lowercase
+      const term = this.state.searchText.trim().toLowerCase();
+
+      // Filter will return a NEW array of contacts, the contact will
+      // be included in the array if the function returns true,
+      // and excluded if the function returns false
+      const filteredContacts = this.state.contacts.filter(contact => {
+        return contact.name.toLowerCase().indexOf(term) >= 0;
+      });
+
+      return filteredContacts;
+    }
+
+  render() {
+      return (
       <div className="App">
-        <SearchBar></SearchBar>
-        <ContactList />
+        <SearchBar onChange={this.handleSearchBarChange.bind(this)} value={this.state.searchText}/>
+        <ContactList contacts={this.getFilteredContacts()} />
       </div>
     );
   }
