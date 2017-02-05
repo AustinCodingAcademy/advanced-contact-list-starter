df57125115d254e6a03724ca2dec097b0664393a
Ray Hansen <RayHansenJr@gmail.com>
2017-02-05 14:53:04 -0600

    loading what i have, not finished though

--- src/App.js
+++ src/App.js
@@ -1,50 +1,70 @@
 import React, { Component } from 'react';
 import ContactList from './ContactList';
 import SearchBar from './SearchBar';
-
+/* eslint-disable max-len */
 class App extends Component {
-  render() {
-    const contacts = [
-      {
-        _id: 1,
-        name: 'Dale Cooper',
-        occupation: 'FBI Agent',
-        avatar: 'https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg'
-      },
-      {
-        _id: 2,
-        name: 'Spike Spiegel',
-        occupation: 'Bounty Hunter',
-        avatar: 'http://vignette4.wikia.nocookie.net/deadliestfiction/images/d/de/Spike_Spiegel_by_aleztron.jpg/revision/latest?cb=20130920231337'
-      },
-      {
-        _id: 3,
-        name: 'Wirt',
-        occupation: 'adventurer',
-        avatar: 'http://66.media.tumblr.com/5ea59634756e3d7c162da2ef80655a39/tumblr_nvasf1WvQ61ufbniio1_400.jpg'
-      },
-      {
-        _id: 4,
-        name: 'Michael Myers',
-        occupation: 'Loving little brother',
-        avatar: 'http://vignette2.wikia.nocookie.net/villains/images/e/e3/MMH.jpg/revision/latest?cb=20150810215746'
-      },
-      {
-        _id: 5,
-        name: 'Dana Scully',
-        occupation: 'FBI Agent',
-        avatar: 'https://pbs.twimg.com/profile_images/718881904834056192/WnMTb__R.jpg'
-      }
-    ];
+  constructor() {
+    super();
 
+    this.state = {
+      searchText: '',
+      contacts: [
+        {
+          _id: 1,
+          name: 'Dale Cooper',
+          occupation: 'FBI Agent',
+          avatar: 'https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg'
+        },
+        {
+          _id: 2,
+          name: 'Spike Spiegel',
+          occupation: 'Bounty Hunter',
+          avatar: 'http://vignette4.wikia.nocookie.net/deadliestfiction/images/d/de/Spike_Spiegel_by_aleztron.jpg/revision/latest?cb=20130920231337'
+        },
+        {
+          _id: 3,
+          name: 'Wirt',
+          occupation: 'adventurer',
+          avatar: 'http://66.media.tumblr.com/5ea59634756e3d7c162da2ef80655a39/tumblr_nvasf1WvQ61ufbniio1_400.jpg'
+        },
+        {
+          _id: 4,
+          name: 'Michael Myers',
+          occupation: 'Loving little brother',
+          avatar: 'http://vignette2.wikia.nocookie.net/villains/images/e/e3/MMH.jpg/revision/latest?cb=20150810215746'
+        },
+        {
+          _id: 5,
+          name: 'Dana Scully',
+          occupation: 'FBI Agent',
+          avatar: 'https://pbs.twimg.com/profile_images/718881904834056192/WnMTb__R.jpg'
+        }
+      ]
+    };
+  }
+
+  handleSearchBarChange(event) {
+    this.setState({
+      searchText: event.target.value
+    });
+  }
+
+  getFilteredContacts() {
+    const term = this.state.searchText.trim().toLowerCase();
+    const filteredContacts = this.state.contacts.filter(contact => {
+      return contact.name.toLowerCase().indexOf(term) >= 0;
+    });
+    return filteredContacts;
+  }
+
+  render() {
     return (
       <div className="App">
-        <ul className="contact-list">
-          {contacts.map(contact => <li key={contact._id}>{contact.name}</li>)}
-        </ul>
+        <SearchBar value={this.state.searchText} onChange={this.handleSearchBarChange.bind(this)} />
+        <ContactList contacts={this.state.contacts} />
       </div>
     );
   }
-}
+ }
 
 export default App;
