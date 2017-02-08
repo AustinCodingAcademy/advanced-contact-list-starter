1945b08ac304e05b0551f4c6c6182d54c316aeda
Ray Hansen <RayHansenJr@gmail.com>
2017-02-07 20:24:41 -0600

    Some Extensions and PropTypes added

--- src/ContactList.js
+++ src/ContactList.js
@@ -2,21 +2,28 @@ import React from 'react';
 import Contact from './Contact';
 
 /* eslint-disable max-len */
-const ContactList = (props) => {
+const ContactList = props => {
   return (
     <ul className="contact-list">
-      {props.contacts.maps(contact => {
+      {props.contacts.map(contact => {
         return (
           <Contact
             key={contact._id}
             name={contact.name}
             avatar={contact.avatar}
             occupation={contact.occupation}
+            _id={contact._id}
+            onClick={props.onClick}
             />
         );
       })}
     </ul>
   );
+};
+
+ContactList.propTypes = {
+  contacts: React.propTypes.func.isRequired,
+  onClick: React.propTypes.string.isRequired
 };
 
 export default ContactList;
