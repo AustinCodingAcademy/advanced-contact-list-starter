012bf288ed4dbf8e7a7337ec048bd06063e089b8
dsolis421 <snapshot8d@gmail.com>
2017-02-28 21:00:13 -0600

    added react router

--- src/App.js
+++ src/App.js
@@ -1,182 +1,22 @@
-import React, {Component} from 'react';
-import ContactList from './ContactList';
-import SearchBar from './SearchBar';
-import ContactForm from './ContactForm';
-import Activity from './Activity';
-import axios from 'axios';
+import React, {Component} from "react";
+import Profile from "./Profile";
+import Contacts from "./Contacts";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 class App extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            searchText: '',
-            favorites: [],
-            contacts: [],
-            activity: [],
-            contactFormVisible: false
-        };
-    }
-
-    componentDidMount() {
-      axios.get('http://localhost:4000/contacts')
-        .then(resp => {
-          this.setState({
-            searchText: this.state.searchText,
-            contacts: resp.data
-          })
-        })
-        .catch(err => {
-          console.log(`Error! ${err}`)
-        })
-      axios.get('http://localhost:4000/favorites')
-        .then(resp => {
-          this.setState({
-            favorites: resp.data
-          })
-        })
-        .catch(err => {
-          console.log(`Error! ${err}`)
-        })
-    }
-
-    handleChange(event) {
-        this.setState({
-            searchText: event.target.value
-        });
-    }
-
-    addToFavorites(_id) {
-        let addCon = this.state.contacts.filter(contact => contact._id === _id);
-        //let newActivity = this.state.activity.push(addCon.name + " added to favorites");
-        console.log(addCon);
-
-        this.handleAddFavorite(...addCon);
-        this.handleDeleteContact(_id);
-    }
-
-    removeFromFavorite(_id) {
-      let remCon = this.state.favorites.filter(contact => contact._id === _id);
-
-      console.log(remCon);
-
-      this.handleAddContact(...remCon);
-      this.handleDeleteFavorite(_id);
-    }
-
-    getFilteredContacts(list) {
-        const TERM = this.state.searchText.trim().toLowerCase();
-        const CONTACTS = list;
-
-        if (!TERM) {
-            return CONTACTS;
-        }
-
-        return CONTACTS.filter(contact => {
-            return contact.name.toLowerCase().indexOf(TERM) >= 0;
-        });
-    }
-
-    handleAddContact(attributes) {
-      axios.post('http://localhost:4000/contacts', attributes)
-      .then((resp) => {
-
-        console.log(resp.data.name);
-
-        this.setState({
-          contacts: [...this.state.contacts, resp.data],
-          activity: [...this.state.activity, resp.data.name + " added to contacts"]
-        });
-      })
-      .catch(err => console.log(err));
-    }
-
-    handleAddFavorite(attributes) {
-      axios.post('http://localhost:4000/favorites', attributes)
-      .then((resp) => {
-
-        console.log(resp.data.name);
-
-        this.setState({
-          favorites: [...this.state.favorites, resp.data],
-          activity: [...this.state.activity, resp.data.name + " added to favorites"]
-        });
-      })
-      .catch(err => console.log(err));
-    }
-
-    handleDeleteContact(_id) {
-      axios.delete(`http://localhost:4000/contacts/${_id}`)
-        .then((resp) => {
-          const newContacts = this.state.contacts.filter(contact => contact._id !== _id);
-
-          this.setState({
-            contacts: newContacts
-          });
-        })
-        .catch(err => console.log(`ERROR! ${err}`));
-    }
-
-    handleDeleteFavorite(_id) {
-      axios.delete(`http://localhost:4000/favorites/${_id}`)
-        .then(() => {
-          const newFavorites = this.state.favorites.filter(contact => contact._id !== _id);
-
-          this.setState({
-            favorites: newFavorites
-          });
-        })
-        .catch(err => console.log(`ERROR! ${err}`));
-    }
-
-    showContactForm() {
-      this.setState({
-        contactFormVisible: true
-      });
-    }
-
-    hideContactForm() {
-      this.setState({
-        contactFormVisible: false
-      });
-    }
-
-    render() {
-        return (
-          <div className="App">
-            <div>
-              <h1>React Contact List</h1>
-            </div>
-            <Activity
-              activity={this.state.activity}
-            />
-            <SearchBar
-              value={this.state.searchText}
-              onChange={this.handleChange.bind(this)}
-              showForm={this.showContactForm.bind(this)}
-            />
-            <div id="show-form" onClick={this.showContactForm.bind(this)}><span className="fa fa-plus"></span></div>
-
-            { this.state.contactFormVisible ? <ContactForm hideForm={this.hideContactForm.bind(this)} onSubmit={this.handleAddContact.bind(this)} /> : null }
-            <div>
-              <ContactList
-                contacts={this.getFilteredContacts(this.state.contacts)}
-                listName='Contacts'
-                buttonText="fa fa-heart-o"
-                handleFav={this.addToFavorites.bind(this)}
-                handleDelete={this.handleDeleteContact.bind(this)}
-              />
-              <ContactList
-                contacts={this.getFilteredContacts(this.state.favorites)}
-                listName='Favorites'
-                buttonText="fa fa-heart"
-                handleFav={this.removeFromFavorite.bind(this)}
-                handleDelete={this.handleDeleteFavorite.bind(this)}
-              />
-            </div>
-          </div>
-        );
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="router">
+          <Switch>
+            <Route exact path="/" component={Contacts} />
+            <Route exact path="/profile/:id" component={Profile} />
+            <Route render={() => <h2>Not Found!</h2>} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/Contact.js
+++ src/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Contact extends React.Component {
   constructor() {
@@ -11,27 +12,36 @@ class Contact extends React.Component {
 
   render() {
     return (
-      <li className="contact">
-        <div className="image-cropper">
-          <img src={this.props.avatar} alt="avatar"/>
-        </div>
-        {this.state.confirmDelete ? <div className="contact-alert">
-          <h3>Delete this contact? <span onClick={() => this.props.handleDelete(this.props.id, this.props.listName.toString().toLowerCase())}>Yes</span> / <span onClick={() => this.setState({confirmDelete: false})}>No</span></h3>
-          </div> : <div className="contact-info">
-            <h2>{this.props.name}</h2>
-            {this.props.occupation}
+      <Link to={`/profile/${this.props.id}`} className="contact-link">
+        <li className="contact">
+          <div className="image-cropper">
+            <img src={this.props.avatar} alt="avatar"/>
           </div>
-        }
+          {this.state.confirmDelete ? <div className="contact-alert">
+            <h3>Delete this contact? <span onClick={() => this.props.handleDelete(this.props.id, this.props.listName.toString().toLowerCase())}>Yes</span> / <span onClick={() => this.setState({confirmDelete: false})}>No</span></h3>
+            </div> : <div className="contact-info">
+              <h2>{this.props.name}</h2>
+              {this.props.occupation}
+            </div>
+          }
 
-        <div className="add-start" onClick={() => this.props.handleFav(this.props.id)}>
-          <span className={this.props.buttonText}></span>
-        </div>
-        <div className="add-start" onClick={() => this.setState({confirmDelete: true})}>
-          <span className="fa fa-minus-square"></span>
-        </div>
-      </li>
+          <div className="add-start" onClick={() => this.props.handleFav(this.props.id)}>
+            <span className={this.props.buttonText}></span>
+          </div>
+          <div className="add-start" onClick={() => this.setState({confirmDelete: true})}>
+            <span className="fa fa-minus-square"></span>
+          </div>
+        </li>
+      </Link>
     );
   }
+}
+
+Contact.propTypes = {
+  id: React.PropTypes.number.isRequired,
+  avatar: React.PropTypes.string.isRequired,
+  name: React.PropTypes.string.isRequired,
+  occupation: React.PropTypes.string.isRequired,
 }
 
 export default Contact;
