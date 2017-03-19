5e6acf55538e6a56468e36e5c786213df7d7b6ef
Giovanna Calderoni <g.c1@me.com>
2017-03-18 22:18:44 -0500

    Fixing Client Side Errors

--- server/index.js
+++ server/index.js
@@ -1,25 +1,25 @@
 /* eslint-disable max-len */
 /* eslint-disable no-console */
-// import express from 'express';
+import express from 'express';
 
-// const app = express();
+const app = express();
 
-// app.use(function (request, response, next) {
-//   response.header('Access-Control-Allow-Origin', '*');
-//   response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
+app.use(function (request, response, next) {
+  response.header('Access-Control-Allow-Origin', '*');
+  response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
-// app.all('/*', (request, response) => {
-//   return response.send(request.params['0']);
-// });
+app.all('/*', (request, response) => {
+  return response.send(request.params['0']);
+});
 
-// const PORT = 3001;
+const PORT = 3001;
 
-// app.listen(PORT, (error) => {
-//   if (error) {
-//     return console.log('Error!!', error);
-//   }
-//   return console.log('Listening on: http://localhost:' + PORT);
-// });
+app.listen(PORT, (error) => {
+  if (error) {
+    return console.log('Error!!', error);
+  }
+  return console.log('Listening on: http://localhost:' + PORT);
+});
 
